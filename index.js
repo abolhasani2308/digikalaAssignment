39b96a5d9ec2e106465d4eae28d0bf2cec20fd20
abolhasani2308 <abolhasani2308@gmail.com>
2024-06-20 15:41:09 +0330

    Set force ltr

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/router/App';
+import {AppRegistry, I18nManager} from 'react-native';
 import {name as appName} from './app.json';
+import App from './src/router/App';
 
 AppRegistry.registerComponent(appName, () => App);
+I18nManager.allowRTL(false);
